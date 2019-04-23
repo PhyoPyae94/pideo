3631ddf0dd1a2c9d8f6bfde540bf7b6bd96f2251
PhyoPyae94 <kirudozack@gmail.com>
2019-04-24 00:02:30 +0630

    adding readme.md and project setup

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="./includes/css/style.css">
+</head>
+<body>
+    <div class="pageContainer">
+        <div class="mastHeadContainer">
+            hello
+        </div>
+        <div class="sideNavContainer">
+
+        </div>
+        <div class="mainSectionContainer">
+        
+        </div>
+    </div>
+</body>
+</html>
