d817565bd4994b5b5a43ec7424d3beed415cb421
PhyoPyae94 <kirudozack@gmail.com>
2019-04-24 23:46:21 +0630

    creating site logo and side menu icon

--- index.php
+++ index.php
@@ -5,16 +5,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+
+    <!-- style -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="./includes/css/style.css">
+
+    <!-- jquery google CDN -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+
+    <!-- bootstrap CDN -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+    <script src="includes/js/custom.js"></script>
+
 </head>
 <body>
     <div id="pageContainer">
         <div id="mastHeadContainer">
-            hello
+            <button class="sideNav">
+                <img src="includes/images/icons/menu.png" alt="">
+            </button>
+
+            <a class="logoContainer" href="index.php">
+                <img src="includes/images/icons/VideoTubeLogo.png" title="logo" alt="site Logo">
+            </a>
         </div>
-        <div id="sideNavContainer">
+
+        <div id="sideNavContainer" style="display:none;">
 
         </div>
+        
         <div id="mainSectionContainer">
             <div id="mainContentContainer">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque saepe nulla unde, earum autem eos aut minima incidunt obcaecati quaerat in, cupiditate aliquam sint rerum, explicabo sapiente maxime accusamus minus.
