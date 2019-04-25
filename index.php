11c4709b36719e4f43a19f4638a6343734b6df4b
PhyoPyae94 <kirudozack@gmail.com>
2019-04-25 12:45:01 +0630

    creating column element

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php require('includes/header.php'); ?>
 
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque saepe nulla unde, earum autem eos aut minima incidunt obcaecati quaerat in, cupiditate aliquam sint rerum, explicabo sapiente maxime accusamus minus.
+<div class="column">
+   
+</div>
 
 <?php require('includes/footer.php'); ?>
